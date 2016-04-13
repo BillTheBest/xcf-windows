cb7501d9b373d176b608c6c6db91385588395e1b
mc007 <asd28071977>
2016-04-13 16:40:12 +0200

    =auto-commit

--- start.js
+++ start.js
@@ -4,20 +4,22 @@ var path = require('path');
 var commander = require('commander');
 var util = require('util');
 var exec = require('child_process');
-var mod_events        = require('events');
 var UTILS_ROOT = path.resolve('../../');//Utils
 var APP_ROOT = path.resolve('../../../../');//Control-Freak
 
 //----------------
 var NGINX_EXE = path.resolve(APP_ROOT+'/nginx');
-var NGINX_ARGS = ["-p",APP_ROOT + "/"];
+var NGINX_ARGS = ["-p", APP_ROOT + path.sep];
 //----------------
 var PHP_CGI = path.resolve(APP_ROOT +'/php/php-cgi');
-var PHP_CGI_ARGS = ["-b","0.0.0.0:9011"];
-
+var PHP_CGI_ARGS = ["-b","127.0.0.1:9011"];
 //----------------
 var DEVICE_SERVER = path.resolve(UTILS_ROOT +'/app/xide/server');
 var DEVICE_SERVER_ARGS = [];
+
+//----------------
+var MONGO_SERVER = path.resolve(APP_ROOT+'/mongo/mongod');
+var MONGO_SERVER_ARGS = ["--smallfiles","--quiet","--dbpath", path.resolve(APP_ROOT + '/data/_MONGO'), "--storageEngine=mmapv1"];
 
 console.log('---start servers');
 
@@ -41,10 +43,16 @@ var options = {
         });
     }
 };
+function mongoReady(){
+    var deviceServer = start(DEVICE_SERVER,DEVICE_SERVER_ARGS,extend({
+        cwd:path.resolve(UTILS_ROOT +'/app/xide')
+    },options));
+}
 function start(path,args,options){
 
-    exec.execFile('chmod',['+x',path]);
+    //exec.execFile('chmod',['+x',path]);
 
+    options.path = path;
     var process = exec.spawn(path, args || [],options, function (err, stdout, stderr) {
 
         if (typeof options.callback === 'function') {
@@ -59,7 +67,12 @@ function start(path,args,options){
     }.bind(this));
 
     process.stdout.on('data',function(data){
-        console.log('stdout data (' + process.pid + '): ' + data);
+
+        var str = data.toString();
+        console.log('stdout data (' + process.pid + '): ' + str);
+        if(str.indexOf("waiting for connections on port")!==-1){
+	        mongoReady();
+	    }
     });
     process.stderr.on('data',function(data){
         console.log('stderr data (' + process.pid + '|' + path  + '): \n' + data);
@@ -83,12 +96,13 @@ var nginx = start(NGINX_EXE,NGINX_ARGS,extend({
     killArgs:['-s', 'stop']
 },options));
 
-var php = start(PHP_CGI,PHP_CGI_ARGS,options);
-
-var deviceServer = start(DEVICE_SERVER,DEVICE_SERVER_ARGS,extend({
-    cwd:UTILS_ROOT +'/app/xide'
+var php = start(PHP_CGI,PHP_CGI_ARGS,extend({
+    cwd:path.resolve(APP_ROOT +'/php/')
 },options));
 
+var mongoServer = start(MONGO_SERVER,MONGO_SERVER_ARGS,extend({
+    cwd:APP_ROOT +''
+},options));
 
 
 /********************************************************************
@@ -96,7 +110,6 @@ var deviceServer = start(DEVICE_SERVER,DEVICE_SERVER_ARGS,extend({
  */
 process.stdin.resume();
 process.on('SIGINT', function() {
-
     for (var i = 0; i < pids.length; i++) {
         var obj = pids[i];
         if(obj.options.kill){
@@ -111,8 +124,8 @@ process.on('SIGINT', function() {
             console.error('error killing ');
         }
     }
-
+    //kill us in latestly 5 secs
     setTimeout(function(){
         process.exit();
-    },2000);
+    },5000);
 });
